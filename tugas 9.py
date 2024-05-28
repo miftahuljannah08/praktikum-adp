b095359beddda8afa37ff3562fbd31c6004f09da
MIFTAHULJANNAH <162961282+miftahuljannah08@users.noreply.github.com>
2024-05-28 22:46:43 +0700

    Add files via upload

---
+++ tugas 9.py
@@ -0,0 +1,36 @@
+import os
+from termcolor import colored,cprint
+os.system("cls")
+
+for i in range(4):
+	cprint(" "*2,"red","on_red",end="")
+	print(end=" ")
+cprint(" "*5,"red","on_red")
+
+for i in range(2):
+	cprint(" "*2,"red","on_red",end="")
+	print(end=" ")
+cprint(" "*2,"red","on_red")
+
+for i in range(3):
+	cprint(" "*2,"red","on_red",end="")
+	print(end=" ")
+cprint(" "*2,"red","on_dark_grey",end="")
+print(end=" ")
+cprint(" "*2,"red","on_red")
+
+for i in range (3):
+	cprint(" "*2,"red","on_red",end="")
+	print(end=" ")
+cprint(" "*2,"red","on_dark_grey",end="")
+print(end=" ")
+cprint(" "*2,"red","on_red")
+cprint(" "*2,"red","on_red",end="")
+
+for i in range (4):
+	print(end=" ")
+cprint(" "*2,"red","on_red",end="")
+print(end=" ")
+cprint(" "*2,"red","on_dark_grey",end="")
+print(end=" ")
+cprint(" "*2,"red","on_red",end="")
